77c77aac734d26673d9727aad893f934b5d7d8ca
shota.web3 <shota.web.2355@gmail.com>
2023-10-01 13:52:55 +0900

    一覧ページの作成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
-import React from "react";
+import React, { FC, ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { LayoutProps } from "@/types/types";
  
-const Layout = () => {
+const Layout: FC<LayoutProps> = ( { children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {/* */}
         <Navbar />
         {/* */}
-        <main></main>
+        <main className="w-full pb-12 px-4">{children}</main>
         {/* */}
         <Footer />
       </div>
