bd93a7da5f3e2439220e6ffb924e4e69fd1d2d0f
shota.web3 <shota.web.2355@gmail.com>
2023-10-01 18:12:11 +0900

    Notion API一覧画面繋こみ

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "prod-files-secure.s3.us-west-2.amazonaws.com", 
+      "images.unsplash.com", 
+      "www.notion.so"]
+  }
 }
 
 module.exports = nextConfig
